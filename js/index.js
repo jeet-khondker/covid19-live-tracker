c8d15e950f40ad41c99a0a500627d1bc0cc18991
Jeet Khondker <jeet.usolutionsgroup@gmail.com>
2020-04-19 11:57:03 +0900

    Fine Tuning & Testing API

--- js/index.js
+++ js/index.js
@@ -1,9 +1,28 @@
 var map;
+var covidData;
 
-function initMap() {
+async function initMap() {
+
+    let response = await axios.get("https://corona.lmao.ninja/v2/countries");
+
+    covidData = response.data;
+
+
+
+
+
+    var tokyo = {
+        lat: 35.6762, 
+        lng: 139.6503
+    };
+
     map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 35.6762, lng: 139.6503},
-    zoom: 8
-});
+        center: tokyo,
+        zoom: 11,
+        mapTypeId: "roadmap",
+        zoomControl: false,
+    });
+
+
 }
 
